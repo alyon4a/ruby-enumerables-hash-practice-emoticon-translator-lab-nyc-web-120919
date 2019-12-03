56e38ce88effa75a21a6377281366647bb85d3b7
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:39:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,8 @@ def get_japanese_emoticon(file_path, emoticon)
   
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  
+  library[:get_meaning][emoticon]
 end
