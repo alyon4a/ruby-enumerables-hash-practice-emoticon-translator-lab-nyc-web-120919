0e2b6f70f6c77264615d244abe0924a75044ef8c
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:36:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,14 +2,13 @@ require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-  puts emoticons
-  puts "================================================================"
+
   result = emoticons.reduce({:get_meaning => {}, :get_emoticon => {}}) do |memo, (key, value)|
     w_emoticon = value[0]
     j_emoticon = value[1]
     memo[:get_meaning][j_emoticon] = key
-    p key 
-    p value 
+    memo[:get_emoticon][w_emoticon] = j_emoticon
+    
     memo 
   end
   result
