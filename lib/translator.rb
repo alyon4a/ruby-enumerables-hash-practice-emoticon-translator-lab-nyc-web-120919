934583db67672df23b831d3691143490107f9330
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:27:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   puts emoticons
   puts "================================================================"
-  result = emoticons.reduce({:get_meaning = [], :get_emoticon = []}) { |pair| puts pair }
+  result = emoticons.reduce({:get_meaning => [], :get_emoticon => []}) { |pair| puts pair }
   
 end
 
