db7f66b7832810aaa85f2ea5ca45842f4d2e7522
Olena Matokhina <olena@matokhina.com>
2019-12-03 03:25:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,8 @@ require "yaml"
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   puts emoticons
-  emoticons.reduce
+  result = emoticons.reduce({:get_meaning = [], :get_emoticon = []}) { |pair| puts pair }
+  
 end
 
 def get_japanese_emoticon(western_emoticon)
